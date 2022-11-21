16bb057b5871631a6ff49b5b431d5d3fd1dba4bf
wkd-woo <wkdwoos@gmail.com>
2022-11-21 21:30:41 +0900

    ~ #1.4 packages, varibales, constants, functions

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func main() {
+	totalLen, upperName := lenAndUpper("jay")
+	fmt.Println(totalLen, upperName)
+}
